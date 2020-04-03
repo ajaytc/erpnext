7ac00d9f3ba5a91e0126a3f7a2fb37401ee03cf3
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-03 11:34:52 +0530

    change app name

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 from frappe import _
 
-app_name = "erpnext"
-app_title = "ERPNext"
-app_publisher = "Frappe Technologies Pvt. Ltd."
+app_name = "modehero"
+app_title = "Modehero"
+app_publisher = "Stack Technologies Pvt. Ltd."
 app_description = """ERP made simple"""
 app_icon = "fa fa-th"
 app_color = "#e74c3c"
