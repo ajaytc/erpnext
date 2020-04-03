cb73c4629182aeee9b17b22170a6ff9c3e6587fb
charanaU <charana.udugama@eng.pdn.ac.lk>
2020-04-03 19:20:00 +0530

    Fabric-Packaging-Trimming

---
+++ erpnext/buying/doctype/composition/composition.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Composition', {
+	// refresh: function(frm) {
+
+	// }
+});
