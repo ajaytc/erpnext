8a378f0abe29a8987e6a62b4462e86df4664bf93
charanaU <charana.udugama@eng.pdn.ac.lk>
2020-04-06 18:04:28 +0530

    Destination Address

---
+++ erpnext/selling/doctype/destination/destination.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2020, Stack Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Destination', {
+	// refresh: function(frm) {
+
+	// }
+
+	
+});
