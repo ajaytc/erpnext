9341dd6ebd3aaaf72780aef5f4d8ad3268dcc984
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-07 18:47:19 +0530

    client order page getting sizes

--- erpnext/www/new-client-order/index.js
+++ erpnext/www/new-client-order/index.js
@@ -1,5 +1,37 @@
+var tablecount = 1;
+
 $('#addtable').click(() => {
     $('#box1').clone().appendTo('#container')
+    tablecount++
 })
 
 $('.close').click(e => console.log($(e.target).parent()))
+
+
+$('#validate').click(() => {
+    let p1 = $('.selected-product')[0]
+    console.log($(p1).find("option:selected").text())
+})
+
+$('.selected-product').change((e) => {
+    product = $(e.target).find("option:selected").text()
+    // frappe.call({
+    //     method: 'frappe.client.get_value',
+    //     args: {
+    //         'doctype': 'Item',
+    //         'filters': {'name': item_code},
+    //         'fieldname': [
+    //             'item_name',
+    //             'web_long_description',
+    //             'description',
+    //             'image',
+    //             'thumbnail'
+    //         ]
+    //     },
+    //     callback: function(r) {
+    //         if (!r.exc) {
+    //             // code snippet
+    //         }
+    //     }
+    // });
+})
--- erpnext/www/new-client-order/index.py
+++ erpnext/www/new-client-order/index.py
@@ -7,5 +7,12 @@ no_cache = 1
 
 
 def get_context(context):
-    context.sizes = frappe.get_all("Sizing",fields=["size"])
+    context.sizes = frappe.get_list("Sizing", fields=["size"])
+    context.products = frappe.get_list("Item")
+
+    context.garmentlabel = frappe.get_list("Garment Label")
+
+    context.roles = frappe.get_roles(frappe.session.user)
+    context.isCustomer = "Customer" in context.roles
+    context.isBrand = "Brand User" in context.roles
     return context
