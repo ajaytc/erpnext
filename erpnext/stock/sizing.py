fcf4c13a6076123ecf70a37359ec0d17e5cc96cc
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-07 22:09:45 +0530

    change sizing table according to selected product

---
+++ erpnext/stock/sizing.py
@@ -0,0 +1,12 @@
+import frappe
+
+
+@frappe.whitelist()
+def getSizes(item):
+    sizes = frappe.db.sql(
+        """ select distinct s.size from tabItem i left join `tabSizing` s on i.sizing = s.parent order by s.idx""")
+    temp = []
+
+    for s in sizes:
+        temp.append(s[0])
+    return {"sizes": temp}
--- erpnext/www/new-client-order/index.js
+++ erpnext/www/new-client-order/index.js
@@ -3,35 +3,68 @@ var tablecount = 1;
 $('#addtable').click(() => {
     $('#box1').clone().appendTo('#container')
     tablecount++
+    $('.selected-product').change(productUpdateCallback)
+
 })
 
 $('.close').click(e => console.log($(e.target).parent()))
 
+const productUpdateCallback = (e) => {
+    // console.log($(e.target).parent().parent().parent().parent().find('.table-section')[0])
+    product = $(e.target).find("option:selected").text()
+    frappe.call({
+        method: 'erpnext.stock.sizing.getSizes',
+        args: {
+            item: product
+        },
+        callback: function (r) {
+            if (!r.exc) {
+                let table = generateSizingTable(r.message.sizes)
+                // console.log(table)
+                $(e.target).parent().parent().parent().parent().find('.table-section').html(table)
+            }
+        }
+    });
+}
+
+$('.selected-product').change(productUpdateCallback)
+
+function generateSizingTable(sizes) {
+
+    let heads = '', inputs = ''
+
+    sizes.map(s => {
+        heads += `<th scope="col">${s}</th>`
+        inputs += `<td><input type="text" class="form-control"></td>`
+    })
+
+    return `
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">{{_("Sizing")}}</th>
+                        ${heads}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="qty">
+                        <th scope="row">{{_("Quantity")}}</th>
+                        ${inputs}
+                    </tr>
+                    <tr class="modified-qty">
+                        <th scope="row">{{_("Modified quantity")}}</th>
+                        ${inputs}
+                    </tr>
+                </tbody>
+            </table>
+    `
+}
+
+const cleartable = () => $('.table-section').html('')
+
 
 $('#validate').click(() => {
     let p1 = $('.selected-product')[0]
-    console.log($(p1).find("option:selected").text())
-})
 
-$('.selected-product').change((e) => {
-    product = $(e.target).find("option:selected").text()
-    // frappe.call({
-    //     method: 'frappe.client.get_value',
-    //     args: {
-    //         'doctype': 'Item',
-    //         'filters': {'name': item_code},
-    //         'fieldname': [
-    //             'item_name',
-    //             'web_long_description',
-    //             'description',
-    //             'image',
-    //             'thumbnail'
-    //         ]
-    //     },
-    //     callback: function(r) {
-    //         if (!r.exc) {
-    //             // code snippet
-    //         }
-    //     }
-    // });
+    console.log($('.table-section'))
 })
