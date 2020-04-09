5b9e4a0a90ff9df43b83966b825deff6b30bc47d
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-09 15:25:11 +0530

    filter items related to users brand

--- erpnext/www/new-client-order/index.py
+++ erpnext/www/new-client-order/index.py
@@ -8,9 +8,8 @@ no_cache = 1
 
 def get_context(context):
     context.sizes = frappe.get_list("Sizing", fields=["size"])
-    brand = frappe.get_doc("User",frappe.session.user)
-
-    context.products = frappe.get_list("Item")
+    brand = frappe.get_doc("User", frappe.session.user).brand_name
+    context.products = frappe.get_list("Item", filters={'brand': brand})
 
     context.garmentlabel = frappe.get_list("Garment Label")
 
