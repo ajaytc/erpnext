7b2c681c644ef3f6fdd45b0bd413e3c8fb9b34f8
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-15 00:00:06 +0530

    product list bug fix

--- erpnext/www/new-client-order/index.py
+++ erpnext/www/new-client-order/index.py
@@ -13,7 +13,7 @@ def get_context(context):
         # context.sizes = frappe.get_list(
         #     "Sizing", fields=["size"], order_by='idx')
         brand = frappe.get_doc("User", frappe.session.user).brand_name
-        context.products = frappe.get_list("Item", filters={'brand': brand})
+        context.products = frappe.get_all("Item", filters={'brand': brand})
 
         context.garmentlabel = frappe.get_list("Garment Label")
 
