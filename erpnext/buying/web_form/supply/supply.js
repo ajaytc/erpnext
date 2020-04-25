faf21fe8b760f4707785bcc73622fc42e0e3a643
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-25 22:32:50 +0530

    add brand to supplier

--- erpnext/buying/web_form/supply/supply.js
+++ erpnext/buying/web_form/supply/supply.js
@@ -10,4 +10,19 @@ frappe.ready(function () {
 			frappe.web_form.set_value('supplier_group', 'Packaging')
 		}
 	}, 1000);
+
+	setTimeout(() => {
+		frappe.call({
+			method: 'erpnext.modehero.user.get_brand',
+			args: {
+				user: frappe.session.user
+			},
+			callback: function (r) {
+				if (!r.exc) {
+					console.log(r.message)
+					frappe.web_form.set_value('brand', r.message)
+				}
+			}
+		});
+	}, 500);
 })
