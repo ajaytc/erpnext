a33490525b758213010b27731a8f65e85970a198
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-25 19:49:32 +0530

    set supplier type

--- erpnext/buying/web_form/supply/supply.js
+++ erpnext/buying/web_form/supply/supply.js
@@ -1,3 +1,8 @@
-frappe.ready(function() {
+frappe.ready(function () {
 	// bind events here
+	let type = "{{ type }}"
+	console.log(type)
+	frappe.web_form.after_load = () => {
+		frappe.web_form.set_value('supplier_group', type)
+	}
 })
--- erpnext/buying/web_form/supply/supply.py
+++ erpnext/buying/web_form/supply/supply.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 
 import frappe
+import json
+
 
 def get_context(context):
-	# do your magic here
-	pass
+    # do your magic here
+    context.type = frappe.form_dict.type
+    pass
