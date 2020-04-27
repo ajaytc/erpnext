7e58550499b919f24f9c93fbc33748518c534b47
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-27 18:38:08 +0530

    auto get item_code

--- erpnext/modehero/product.py
+++ erpnext/modehero/product.py
@@ -5,3 +5,10 @@ import json
 @frappe.whitelist()
 def get_products_of_category(category):
     return frappe.get_list('Item', filters={'item_group': category}, fields=['name', 'item_name'])
+
+
+@frappe.whitelist()
+def get_item_code():
+    items = frappe.get_list(
+        'Item', order_by='creation desc', fields=['item_code'])
+    return int(items[0].item_code)+1
--- erpnext/stock/web_form/products/products.js
+++ erpnext/stock/web_form/products/products.js
@@ -1,15 +1,27 @@
 frappe.ready(function () {
 	// bind events here
 	frappe.call({
-		method: 'erpnext.modehero.user.get_brand',
-		args: {
-			user: frappe.session.user
-		},
+		method: 'erpnext.modehero.product.get_item_code',
+		args: {},
 		callback: function (r) {
 			if (!r.exc) {
 				console.log(r.message)
-				frappe.web_form.set_value('brand', r.message)
+				frappe.web_form.set_value('item_code', r.message)
 			}
 		}
 	});
+	setTimeout(() => {
+		frappe.call({
+			method: 'erpnext.modehero.user.get_brand',
+			args: {
+				user: frappe.session.user
+			},
+			callback: function (r) {
+				if (!r.exc) {
+					console.log(r.message)
+					frappe.web_form.set_value('brand', r.message)
+				}
+			}
+		});
+	}, 500);
 })
