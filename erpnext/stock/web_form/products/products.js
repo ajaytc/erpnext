215ecd924aaf2309dec13660486b1cfd5f0a267a
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-27 18:40:55 +0530

    bug

--- erpnext/stock/web_form/products/products.js
+++ erpnext/stock/web_form/products/products.js
@@ -1,16 +1,17 @@
 frappe.ready(function () {
+	console.log(frappe.web_form)
 	// bind events here
-	frappe.call({
-		method: 'erpnext.modehero.product.get_item_code',
-		args: {},
-		callback: function (r) {
-			if (!r.exc) {
-				console.log(r.message)
-				frappe.web_form.set_value('item_code', r.message)
-			}
-		}
-	});
 	setTimeout(() => {
+		frappe.call({
+			method: 'erpnext.modehero.product.get_item_code',
+			args: {},
+			callback: function (r) {
+				if (!r.exc) {
+					console.log(r.message)
+					frappe.web_form.set_value('item_code', r.message)
+				}
+			}
+		});
 		frappe.call({
 			method: 'erpnext.modehero.user.get_brand',
 			args: {
