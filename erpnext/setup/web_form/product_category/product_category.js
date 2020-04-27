aef1d9c669d96a49e46813ef7ec58e6d88794952
Dinuka Salwathura <dinukasal@gmail.com>
2020-04-27 20:38:10 +0530

    bug fix

--- erpnext/setup/web_form/product_category/product_category.js
+++ erpnext/setup/web_form/product_category/product_category.js
@@ -10,7 +10,7 @@ frappe.ready(function () {
 			callback: function (r) {
 				if (!r.exc) {
 					console.log(r.message)
-					frappe.web_form.set_value('brand', r.message)
+					frappe.web_form.set_value('brand_name', r.message)
 				}
 			}
 		});
