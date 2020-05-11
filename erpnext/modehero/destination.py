bdc38f59caba43f250bac76b2e0702695a4657f7
Dinuka Salwathura <dinukasal@gmail.com>
2020-05-11 18:07:43 +0530

    destination bug fix

--- erpnext/modehero/destination.py
+++ erpnext/modehero/destination.py
@@ -7,7 +7,7 @@ def create_destination(data):
     data = json.loads(data)
     item = frappe.get_doc({
         'doctype': 'Destination',
-        'destination_name': data['name']
+        'destination_name': data['name'],
         'email_address': data['email'],
         'address_line_1': data['address1'],
         'address_line_2': data['address2'],
