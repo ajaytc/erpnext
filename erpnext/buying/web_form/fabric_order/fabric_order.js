7dde8d238d2c67f458bd1692d618e974432831e7
Dinuka Salwathura <dinukasal@gmail.com>
2020-05-13 12:46:41 +0530

    remove brand from forms

--- erpnext/buying/web_form/fabric_order/fabric_order.js
+++ erpnext/buying/web_form/fabric_order/fabric_order.js
@@ -1,17 +1,4 @@
 frappe.ready(function() {
 	// bind events here
-	setTimeout(() => {
-		frappe.call({
-			method: 'erpnext.modehero.user.get_brand',
-			args: {
-				user: frappe.session.user
-			},
-			callback: function (r) {
-				if (!r.exc) {
-					console.log(r.message)
-					frappe.web_form.set_value('brand', r.message)
-				}
-			}
-		});
-	}, 500);
+
 })
--- erpnext/buying/web_form/supply/supply.js
+++ erpnext/buying/web_form/supply/supply.js
@@ -11,18 +11,5 @@ frappe.ready(function () {
 		}
 	}, 1000);
 
-	setTimeout(() => {
-		frappe.call({
-			method: 'erpnext.modehero.user.get_brand',
-			args: {
-				user: frappe.session.user
-			},
-			callback: function (r) {
-				if (!r.exc) {
-					console.log(r.message)
-					frappe.web_form.set_value('brand', r.message)
-				}
-			}
-		});
-	}, 500);
+
 })
