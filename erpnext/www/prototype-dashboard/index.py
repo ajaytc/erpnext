a0da40c029eaf83e451da00779f849564677bc27
Dinuka Salwathura <dinukasal@gmail.com>
2020-05-22 13:50:40 +0530

    bug fixes 22

--- erpnext/www/prototype-dashboard/index.py
+++ erpnext/www/prototype-dashboard/index.py
@@ -15,13 +15,14 @@ def get_context(context):
     #         _("You need to be logged in to access this page"), frappe.PermissionError)
 
     context.show_sidebar = False
+    brand = frappe.get_doc("User", frappe.session.user).brand_name
 
-    context.onprocess = frappe.get_list(
-        'Prototype Order', filters={'docstatus': 0}, fields=['name', 'internal_ref', 'product', 'product_category', 'quantity_per_size', 'creation', 'ex_work_date', 'tracking_number'])
-    context.validate = frappe.get_list(
-        'Prototype Order', filters={'docstatus': 1}, fields=['name', 'internal_ref', 'product', 'product_category', 'quantity_per_size', 'creation', 'tracking_number'])
-    context.notvalidate = frappe.get_list(
-        'Prototype Order', filters={'docstatus': 2}, fields=['name', 'internal_ref', 'product', 'product_category'])
+    context.onprocess = frappe.get_all(
+        'Prototype Order', filters={'docstatus': 0, 'brand': brand}, fields=['name', 'internal_ref', 'product', 'product_category', 'quantity_per_size', 'creation', 'ex_work_date', 'tracking_number'])
+    context.validate = frappe.get_all(
+        'Prototype Order', filters={'docstatus': 1, 'brand': brand}, fields=['name', 'internal_ref', 'product', 'product_category', 'quantity_per_size', 'creation', 'tracking_number'])
+    context.notvalidate = frappe.get_all(
+        'Prototype Order', filters={'docstatus': 2, 'brand': brand}, fields=['name', 'internal_ref', 'product', 'product_category'])
 
     # context.parents = [
     #     {"name": frappe._("Home"), "route": "/"}
