fe515a77f4a486cd6e07489ca1dd26f8a0e70b15
charlie9526 <asansandeepa@gmail.com>
2020-07-09 02:17:54 +0530

    purchase list new page- developing yet

--- erpnext/www/purchase/index.js
+++ erpnext/www/purchase/index.js
@@ -1,7 +1,19 @@
 function select_all_chekbox(item) {
-    if ($(this).is(':checked')) {
+    if ($('.select-all-sales-orders-'+item).is(':checked')) {
         $('.sales-order-checkbox-'+item).prop('checked', true);
     } else {
         $('.sales-order-checkbox-'+item).prop('checked', false);
     }
+}
+
+function set_sum(itm_code,size){
+    var sum = 0
+    $("."+itm_code+"-"+size).each(function() {
+        sum = sum + Number($( this ).text());
+    });
+    $("."+itm_code+"-"+size+"-sum").text(sum)
+}
+
+function ss(){
+    console.log(123)
 }
