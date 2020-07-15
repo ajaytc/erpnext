3ccc627a788008e535ffccd8be00b07137c9ba5f
charlie9526 <asansandeepa@gmail.com>
2020-07-15 16:18:41 +0530

    point of sales doctype and web form
    Signed-off-by: charlie9526 <asansandeepa@gmail.com>

--- erpnext/modehero/customer.py
+++ erpnext/modehero/customer.py
@@ -7,3 +7,15 @@ def get_customer(email):
         return frappe.get_all('Customer', filters={'email_address': email})[0].name
     except:
         frappe.throw(frappe._("Error"))
+
+@frappe.whitelist()
+def get_branded_companies():
+    brand = frappe.get_doc('User', frappe.session.user).brand_name
+    company_objs = frappe.get_all('Customer',filters={'brand':brand},fields=['name'])
+    result = []
+    for x in company_objs:
+        result.append({
+            'label':x.name,
+            'value':x.name
+        })
+    return result
