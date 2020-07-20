09c0059ff3498422af37a50b9418824c7b30733e
charlie9526 <asansandeepa@gmail.com>
2020-07-20 17:27:14 +0530

    Pricing Attribution
    Signed-off-by: charlie9526 <asansandeepa@gmail.com>

--- erpnext/modehero/customer.py
+++ erpnext/modehero/customer.py
@@ -1,4 +1,6 @@
 import frappe
+import json
+import ast
 
 
 @frappe.whitelist()
@@ -19,3 +21,52 @@ def get_branded_companies():
             'value':x.name
         })
     return result
+
+@frappe.whitelist()
+def set_pricing(form_data):
+    form_data = json.loads(form_data)
+    # here the consisency of the from to values are checked.
+    # wholesale_prices = check_wholesale_correct(form_data["wholesale_price"])
+    user = frappe.get_doc('User', frappe.session.user)
+    brand = user.brand_name
+    pricing = form_data["client"] + " " + form_data["season"]
+    pricing = frappe.get_doc(
+        {
+            "doctype": "Client Pricing",
+            "brand": brand,
+            "client": form_data["client"],
+            "item_group": form_data["item_group"],
+            "item_code": form_data["item_code"],
+            "season": form_data["season"],
+            "minimum_order": form_data["minimum_order"],
+            "show_price": form_data["show_price"],
+            "wholesale_price":form_data["wholesale_price"],
+            "pricing_options":form_data["pricing_options"],
+            "pricing_name":pricing
+         })
+
+    pricing.insert(ignore_permissions=True)
+    frappe.db.commit()
+    return {'status': 'ok'}
+
+
+# def check_wholesale_correct(wholesale_dicts):
+#     is_any_wrong = False
+#     temp_num_checker = []
+#     wholesale_dicts.sort(key=get_from)
+#     for dic in wholesale_dicts:
+#         if (len(temp_num_checker)==0):
+#             temp_num_checker.append(dic["from"])
+#             temp_num_checker.append(dic["to"])
+#             continue
+#         length_temp = len(temp_num_checker)
+#         if (temp_num_checker[length_temp-2]>temp_num_checker[length_temp-1] | dic["from"]!=temp_num_checker[length_temp-1]+1 ):
+#             is_any_wrong = True
+#             break
+#     if (is_any_wrong):
+#         return False
+#     return True
+
+# def get_from(wholesale_dict):
+#     return wholesale_dict['from']
+    
