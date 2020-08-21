838341c0fd44e8af9d37e74d85345f1e68262b17
Dhananjana <sachinthadhananjana.16@cse.mrt.ac.lk>
2020-08-21 18:33:08 +0530

    uniform order creation ok dispaly stock remain

--- erpnext/modehero/uniform.py
+++ erpnext/modehero/uniform.py
@@ -26,10 +26,106 @@ def get_products_of_package(packageName):
         productName=product.item_code
         productOb=frappe.get_doc('Item',productName)
         prod={
-            'prod_name':productOb.item_name,
-            'qty':product.quantity
+            'item_name':productOb.item_name,
+            'qty':product.quantity,
+            'item_code':productOb.name
         };
         productDetails.append(prod)
 
 
     return productDetails
+
+@frappe.whitelist()
+def createUniformOrder(data):
+    print('dddddd')
+    data = json.loads(data)
+    # uniOrder=frappe.get_doc('Uniform Order','43aeef2abf')
+
+    user = frappe.get_doc('User', frappe.session.user)
+    brand = user.brand_name
+    # orderSegment=frappe.get_doc({
+    #     'doctype':'Uniform order Segment',
+    #     'reciever_name':data['']
+
+    # })
+    segments=[]
+    for segment in data['segments']:
+        recieverName=segment['name']
+        productDetails=[]
+
+        for productDetail in segment['segmentProducts']:
+            product={}
+            product['item_code']=productDetail['item_code']
+            product['order_no']=productDetail['orderNum']
+            product['quantity']=productDetail['qty']
+            product['size']=productDetail['size']
+            product['comment']=productDetail['comment']
+
+            productDetails.append(product)
+        
+
+        segment={}
+        segment['reciever_name']=recieverName
+        segment['segment_products']=productDetails
+
+        
+        segments.append(segment)
+
+
+    order=frappe.get_doc({
+        'doctype':'Uniform Order',
+        'brand': brand,
+        'customer':data['client'],
+        'point_of_safe':data['pos'],
+        'package':data['package'],
+        'order_segments':segments
+    })
+
+    order.insert()
+    frappe.db.commit()
+    updateSegmentProductDetails(data,order)
+
+    return order
+
+def updateSegmentProductDetails(data,order):
+
+    # frappe.get_all('Uniform order Segment',filters={'parent':order.name},fields=['name','reciever_name'])
+    segments=order.order_segments
+
+    for segment in segments:
+        for product in segment.segment_products:
+            product.parent=segment.name
+            product.insert(ignore_permissions=True)
+        
+    frappe.db.commit()
+
+
+    # for segmentIdx in range(0,len(data['segments'])) :
+        
+    #     recieverName=segment['name']
+        # productDetails=[]
+
+        # for productDetail in segment['segmentProducts']:
+        #     product={}
+        #     product['item_code']=productDetail['item_code']
+        #     product['order_no']=productDetail['orderNum']
+        #     product['quantity']=productDetail['qty']
+        #     product['size']=productDetail['size']
+
+        #     productDetails.append(product)
+        
+
+        # segment={}
+        # segment['reciever_name']=recieverName
+        # segment['segment_products']=productDetails
+
+    # order = frappe.get_doc({
+    #     'doctype': 'Fabric Order',
+    #     'brand': brand,
+    #     'fabric_vendor': data['fabric_vendor'],
+    #     'internal_ref': data['internal_ref'],
+    #     'fabric_ref': data['fabric_ref'],
+    #     'product_name': data['item_code'],
+
+
+
--- erpnext/www/uniform/index.js
+++ erpnext/www/uniform/index.js
@@ -10,7 +10,7 @@ function generateOptions(values, val_key, txt_key) {
 $('#client-name-select').change(function () {
     makePOSList($(this))
     makePackageList($(this))
-    
+
 
 })
 
@@ -63,45 +63,167 @@ $('#package-select').change(function () {
                 html = ``
                 firstFlag = true
                 namePart = `<div class="col-1" style="padding-top: 2.6%;">
-                    <label>{{_("Name")}}</label>
+                    <label>{{_("Name*")}}</label>
                     <input class="form-control clientname-select" id="client-name-select">
                 </div>`
-                html=namePart
+                html = namePart
                 $.each(products, function (index, value) {
-                        html = html + `
-                <div class="col-2" style="text-align: center;flex: 0 0 12.667%;padding-left:2%">
-                    <label style="margin-top: 4%;">{{_("`+ value['prod_name'] + `")}}</label>
+                    html = html + `
+                <div class="col-2 seg-products" style="text-align: center;flex: 0 0 12.667%;padding-left:2%">
+                    <label style="margin-top: 4%;">{{_("`+ value['item_name'] + `")}}</label><span style="text-align: right !important;padding-left:20px" id="com">
+                    <a type="button" id="addFromStock" style="">+</a>
+                </span><span><a type="button" class="dha"  style="padding-left:5px">!</a></span>
+                <hr style="margin-top:0px;margin-bottom:0px">
+                    <label style="margin-top: 4%;display:none" id="item_code">{{_("`+ value['item_code'] + `")}}</label>
                     <div class="row">
                         <div class="col-4">
-                            <label>{{_("Order#")}}</label>
-                            <input class="form-control clientname-select" id="client-name-select">
+                            <label>{{_("Order#*")}}</label>
+                            <input class="form-control product-select" id="orderNum">
                         </div>
                         <div class="col-4">
-                            <label>{{_("Qty")}}</label>
-                            <input class="form-control clientname-select" value="`+ value['qty'] + `" id="client-name-select">
+                            <label>{{_("Qty*")}}</label>
+                            <input class="form-control" disabled value="`+ value['qty'] + `" id="qty">
 
                         </div>
                         <div class="col-4">
-                            <label>{{_("Size")}}</label>
-                            <input class="form-control clientname-select" id="client-name-select">
+                            <label>{{_("Size*")}}</label>
+                            <input class="form-control product-select" id="size">
 
                         </div>
                     </div>
+                    <div style="display: none;" id="commentdiv">
+                            <label>{{_("Comment")}}</label>
+                            <input type="text" class="form-control clientname-select" id="commentField">
+                        </div>
 
                 </div>
                 
                 `
-                    
+
 
 
                 })
-                $('.product').html('')
-                $('#products').html(html)
+                $('.segmentPart').html('')
+                $('#segment').html(html)
             }
         }
     })
 })
-
+$(document).ready(function(){
+    $(document).on("click", "a.dha" , function() {
+        if($(this).parent().parent().find('#commentdiv').is(":hidden")){
+            $(this).parent().parent().find('#commentdiv').show()
+        }else{
+            $(this).parent().parent().find('#commentdiv').hide()
+        }
+        
+    });
+});
+// $("a").on("click",".dha",function () {
+//     $(this).find('#commentdiv').show()
+// })
 $('#addSegment').click(function () {
-    $('#products').first().clone(true).appendTo($("#seg"))
+    $('#segment').first().clone(true).appendTo($("#segs"))
+    addedSeg = $('.segmentPart').last()
+    $(addedSeg).find('.product-select').val('')
+    $(addedSeg).find('#client-name-select').val('')
+    $(addedSeg).find('#commentdiv').hide()
+    // $(addedSeg).find('#size').val('')
+
+
+
 })
+
+$('#uniformSubmit').click(function () {
+    customer = $('#client-name-select').val()
+    pos = $('#pos-select').val()
+    packageName = $('#package-select').val()
+    segments = []
+    notCompleted = false
+
+
+    $('.segmentPart').each(function (index, value) {
+        console.log(value)
+        name = $(value).find('#client-name-select').val()
+        if (name == '') {
+            notCompleted = true
+            return giveError()
+
+        }
+        segment = {}
+        segment['name'] = name
+        segmentProducts = []
+        $(value).find('.seg-products').each(function (index, value) {
+            orderNum = $(value).find('#orderNum').val()
+            qty = $(value).find('#qty').val()
+            size = $(value).find('#size').val()
+            comment=$(value).find('#commentField').val()
+            item_code = $(value).find('#item_code').text()
+            if (orderNum == '' || qty == '' || size == '') {
+                notCompleted = true
+                return giveError()
+
+            }
+
+            segmentProduct = {}
+            segmentProduct['orderNum'] = orderNum
+            segmentProduct['qty'] = qty
+            segmentProduct['size'] = size
+            segmentProduct['comment'] = comment
+            segmentProduct['item_code'] = item_code
+
+            segmentProducts.push(segmentProduct)
+
+        })
+        segment['segmentProducts'] = segmentProducts
+        segments.push(segment)
+
+
+
+    })
+
+    if (!notCompleted) {
+        frappe.call({
+            method: 'erpnext.modehero.uniform.createUniformOrder',
+            args: {
+                data: {
+                    'client': customer,
+                    'pos': pos,
+                    'package': packageName,
+                    'segments': segments
+                }
+            },
+            callback: function (r) {
+                if (!r.exc) {
+                    console.log(r.message)
+                    frappe.msgprint({
+                        title: __('Notification'),
+                        indicator: 'green',
+                        message: __('Uniform order created successfully')
+                    });
+                } else {
+                    frappe.msgprint({
+                        title: __('Notification'),
+                        indicator: 'red',
+                        message: __('Uniform order creation failed')
+                    });
+                }
+            }
+        })
+    }
+
+
+
+
+
+
+})
+
+function giveError() {
+    frappe.msgprint({
+        title: __('Notification'),
+        indicator: 'red',
+        message: __('Please fill all mandetory fields')
+    });
+
+}
